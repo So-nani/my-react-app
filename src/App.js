75975bbd8f08216ab68513162db2cee1acda0883
So-nani <sonani3136@gmail.com>
2025-08-16 23:51:48 +0900

    List
    asd

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDoList, setToDoList] = useState([]);
+  const onChange = (e) => {
+    setToDo(e.target.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDoList((currentArray) => [toDo, ...currentArray]);
+  };
+  console.log(toDoList);
   return (
     <div>
-      <h1>Welcome back!</h1>
+      <h1>My ToDos({toDoList.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your To-Do..."
+        ></input>
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDoList.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
