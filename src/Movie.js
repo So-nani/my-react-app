539c9f226f0730e3716cebd303b33cb139605958
So-nani <sonani3136@gmail.com>
2025-08-17 17:44:29 +0900

    movie
    asd

---
+++ src/Movie.js
@@ -0,0 +1,5 @@
+import { useState, useEffect } from "react";
+
+function Movie() {}
+
+export default Movie;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Movie from "./Movie";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
   <App />
+  // <Movie />
   // </React.StrictMode>
 );
